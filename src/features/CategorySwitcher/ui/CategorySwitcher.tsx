be082918f22251b443c7fedb7578e54a72c70063
yukine <emilyroads232@gmail.com>
2025-06-21 02:07:31 +0300

    upd2

--- src/features/CategorySwitcher/ui/CategorySwitcher.tsx
+++ src/features/CategorySwitcher/ui/CategorySwitcher.tsx
@@ -8,20 +8,22 @@ type Props = {
 
 export default function CategorySwitcher({ type = 'default' }: Props) {
   return (
-    <ul
+    <div
+      role='menu'
+      aria-label='Список категорий'
       className={`flex items-start ${
         type === 'default'
           ? 'w-[649px] h-[38px] mt-[80px]'
           : 'w-[536px] h-[34px] mb-[30px]'
       } gap-[10px] select-none`}
-      role='list'
-      aria-label='Список категорий'
     >
       {CATEGORIES.map((category, index) => (
-        <li role='listitem' key={index}>
-          <Category title={category.title as CategoryType} type={type} />
-        </li>
+        <Category
+          key={index}
+          title={category.title as CategoryType}
+          type={type}
+        />
       ))}
-    </ul>
+    </div>
   )
 }
